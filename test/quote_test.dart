66d6d31b1126a4557c0ef6e6b4bb99b11f75d563
liplum <Li_plum@outlook.com>
2025-02-13 13:03:27 +0800

    Refactor: Improve boolean assertions in quote_test.dart
    - Changed assertions from `equals(true)` or `equals(false)` to `isTrue` and `isFalse` respectively, for better readability.

--- test/quote_test.dart
+++ test/quote_test.dart
@@ -4,132 +4,132 @@ import 'package:yaml_writer/src/node.dart';
 void main() {
   group('Unquoted', () {
     test("simple text", () {
-      expect(StringNode.isValidUnquotedString("foo"), equals(true));
-      expect(StringNode.isValidUnquotedString("FOO"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo-bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("foo"), isTrue);
+      expect(StringNode.isValidUnquotedString("FOO"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo-bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo bar"), isTrue);
     });
 
     test("numbers", () {
       // numbers are invalid because yaml would consider them as numbers other than strings
-      expect(StringNode.isValidUnquotedString("1"), equals(false));
-      expect(StringNode.isValidUnquotedString("1.5"), equals(false));
-      expect(StringNode.isValidUnquotedString("-1"), equals(false));
-      expect(StringNode.isValidUnquotedString("-1.5"), equals(false));
+      expect(StringNode.isValidUnquotedString("1"), isFalse);
+      expect(StringNode.isValidUnquotedString("1.5"), isFalse);
+      expect(StringNode.isValidUnquotedString("-1"), isFalse);
+      expect(StringNode.isValidUnquotedString("-1.5"), isFalse);
     });
 
     test("i18n", () {
       // Good for translated strings
-      expect(StringNode.isValidUnquotedString("Hello"), equals(true));
-      expect(StringNode.isValidUnquotedString("你好"), equals(true));
-      expect(StringNode.isValidUnquotedString("こんにちは"), equals(true));
-      expect(StringNode.isValidUnquotedString("Bonjour"), equals(true));
+      expect(StringNode.isValidUnquotedString("Hello"), isTrue);
+      expect(StringNode.isValidUnquotedString("你好"), isTrue);
+      expect(StringNode.isValidUnquotedString("こんにちは"), isTrue);
+      expect(StringNode.isValidUnquotedString("Bonjour"), isTrue);
     });
 
     test("at sign", () {
-      expect(StringNode.isValidUnquotedString("@foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo@bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo @ bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("@foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo@bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo @ bar"), isTrue);
     });
 
     test("question mark", () {
-      expect(StringNode.isValidUnquotedString("?foo"), equals(true));
+      expect(StringNode.isValidUnquotedString("?foo"), isTrue);
     });
 
     test("dash", () {
-      expect(StringNode.isValidUnquotedString("-foo"), equals(true));
-      expect(StringNode.isValidUnquotedString("-"), equals(false));
-      expect(StringNode.isValidUnquotedString("- "), equals(false));
-      expect(StringNode.isValidUnquotedString("foo-"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo-bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("-foo"), isTrue);
+      expect(StringNode.isValidUnquotedString("-"), isFalse);
+      expect(StringNode.isValidUnquotedString("- "), isFalse);
+      expect(StringNode.isValidUnquotedString("foo-"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo-bar"), isTrue);
     });
 
     test("brackets", () {
-      expect(StringNode.isValidUnquotedString(r"["), equals(false));
-      expect(StringNode.isValidUnquotedString(r"{"), equals(false));
-      expect(StringNode.isValidUnquotedString(r">"), equals(false));
-      expect(StringNode.isValidUnquotedString(r"[ "), equals(false));
-      expect(StringNode.isValidUnquotedString(r"{ "), equals(false));
-      expect(StringNode.isValidUnquotedString(r"> "), equals(false));
-      expect(StringNode.isValidUnquotedString(r"foo["), equals(true));
-      expect(StringNode.isValidUnquotedString(r"foo{"), equals(true));
-      expect(StringNode.isValidUnquotedString(r"foo>"), equals(true));
+      expect(StringNode.isValidUnquotedString(r"["), isFalse);
+      expect(StringNode.isValidUnquotedString(r"{"), isFalse);
+      expect(StringNode.isValidUnquotedString(r">"), isFalse);
+      expect(StringNode.isValidUnquotedString(r"[ "), isFalse);
+      expect(StringNode.isValidUnquotedString(r"{ "), isFalse);
+      expect(StringNode.isValidUnquotedString(r"> "), isFalse);
+      expect(StringNode.isValidUnquotedString(r"foo["), isTrue);
+      expect(StringNode.isValidUnquotedString(r"foo{"), isTrue);
+      expect(StringNode.isValidUnquotedString(r"foo>"), isTrue);
     });
 
     test("whitespace", () {
-      expect(StringNode.isValidUnquotedString(" "), equals(false));
-      expect(StringNode.isValidUnquotedString(" foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo "), equals(false));
-      expect(StringNode.isValidUnquotedString("foo\nbar"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo\rbar"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo\tbar"), equals(false));
+      expect(StringNode.isValidUnquotedString(" "), isFalse);
+      expect(StringNode.isValidUnquotedString(" foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo "), isFalse);
+      expect(StringNode.isValidUnquotedString("foo\nbar"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo\rbar"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo\tbar"), isFalse);
     });
 
     test("star", () {
-      expect(StringNode.isValidUnquotedString("*foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("*"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo*"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo*bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("*foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("*"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo*"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo*bar"), isTrue);
     });
 
     test("ampersand", () {
-      expect(StringNode.isValidUnquotedString("&foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("&"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo&"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo&bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("&foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("&"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo&"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo&bar"), isTrue);
     });
 
     test("comma", () {
-      expect(StringNode.isValidUnquotedString(",foo"), equals(false));
-      expect(StringNode.isValidUnquotedString(","), equals(false));
-      expect(StringNode.isValidUnquotedString("foo,"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo,bar"), equals(true));
+      expect(StringNode.isValidUnquotedString(",foo"), isFalse);
+      expect(StringNode.isValidUnquotedString(","), isFalse);
+      expect(StringNode.isValidUnquotedString("foo,"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo,bar"), isTrue);
     });
 
     test("colon", () {
-      expect(StringNode.isValidUnquotedString(":foo"), equals(true));
-      expect(StringNode.isValidUnquotedString(":"), equals(false));
-      expect(StringNode.isValidUnquotedString(": "), equals(false));
-      expect(StringNode.isValidUnquotedString("foo:"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo:bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo: "), equals(false));
-      expect(StringNode.isValidUnquotedString("foo: bar"), equals(false));
+      expect(StringNode.isValidUnquotedString(":foo"), isTrue);
+      expect(StringNode.isValidUnquotedString(":"), isFalse);
+      expect(StringNode.isValidUnquotedString(": "), isFalse);
+      expect(StringNode.isValidUnquotedString("foo:"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo:bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo: "), isFalse);
+      expect(StringNode.isValidUnquotedString("foo: bar"), isFalse);
     });
 
     test("pipe", () {
-      expect(StringNode.isValidUnquotedString("|foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("|"), equals(false));
-      expect(StringNode.isValidUnquotedString("| foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo|"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo |"), equals(true));
+      expect(StringNode.isValidUnquotedString("|foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("|"), isFalse);
+      expect(StringNode.isValidUnquotedString("| foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo|"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo |"), isTrue);
     });
 
     test("hash", () {
-      expect(StringNode.isValidUnquotedString("#"), equals(false));
-      expect(StringNode.isValidUnquotedString("#foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("# foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo#"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo #"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo #bar"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo # bar"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo# bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("#"), isFalse);
+      expect(StringNode.isValidUnquotedString("#foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("# foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo#"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo #"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo #bar"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo # bar"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo# bar"), isTrue);
     });
 
     test("percentage", () {
-      expect(StringNode.isValidUnquotedString("%foo"), equals(false));
-      expect(StringNode.isValidUnquotedString("%"), equals(false));
-      expect(StringNode.isValidUnquotedString("foo%"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo%bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo% bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo % bar"), equals(true));
-      expect(StringNode.isValidUnquotedString("foo %bar"), equals(true));
+      expect(StringNode.isValidUnquotedString("%foo"), isFalse);
+      expect(StringNode.isValidUnquotedString("%"), isFalse);
+      expect(StringNode.isValidUnquotedString("foo%"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo%bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo% bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo % bar"), isTrue);
+      expect(StringNode.isValidUnquotedString("foo %bar"), isTrue);
     });
 
     test("builtin literal", () {
-      expect(StringNode.isValidUnquotedString("null"), equals(false));
-      expect(StringNode.isValidUnquotedString("true"), equals(false));
-      expect(StringNode.isValidUnquotedString("false"), equals(false));
-      expect(StringNode.isValidUnquotedString("~"), equals(false));
+      expect(StringNode.isValidUnquotedString("null"), isFalse);
+      expect(StringNode.isValidUnquotedString("true"), isFalse);
+      expect(StringNode.isValidUnquotedString("false"), isFalse);
+      expect(StringNode.isValidUnquotedString("~"), isFalse);
     });
   });
 }
